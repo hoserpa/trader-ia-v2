750095de02492f876ce7f9fa49fbc70de22d6704
Jose Ramon Alvarez <joraalgo@gmail.com>
2026-03-25 14:08:05 +0100

    Fix: use ccxt.async_support for async fetch_ohlcv

--- training/fetch_historical_data.py
+++ training/fetch_historical_data.py
@@ -7,7 +7,7 @@ import argparse
 import asyncio
 from datetime import datetime, timedelta
 from pathlib import Path
-import ccxt
+import ccxt.async_support as ccxt
 import pandas as pd
 from loguru import logger
 
